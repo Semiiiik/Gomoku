61defcafca83fb8a7ca675a14507614d65c7dfba
Semiiiik <mirda.semik@gmail.com>
2026-08-16 19:14:05 +0200

    added swap + swap2

--- game.js
+++ game.js
@@ -2,8 +2,29 @@
 const boardCanvas = document.getElementById('board');
 const gameEndModal = document.getElementById('gameEndModal');
 document.getElementById("playAgainButton").addEventListener("click", startGame);
+document.getElementById("player1ChooseBlack").addEventListener("click", swapChooseBlack);
+document.getElementById("player1ChooseBlack").addEventListener("click", enableBoardInteraction);
+document.getElementById("player1ChooseWhite").addEventListener("click", swapChooseWhite);
+document.getElementById("player1ChooseWhite").addEventListener("click", enableBoardInteraction);
+document.getElementById("player1PlaceSwap2").addEventListener("click", swapChoose2);
+document.getElementById("player1PlaceSwap2").addEventListener("click", enableBoardInteraction);
+document.getElementById("player2ChooseBlack").addEventListener("click", swapChooseBlack);
+document.getElementById("player2ChooseBlack").addEventListener("click", enableBoardInteraction);
+document.getElementById("player2ChooseWhite").addEventListener("click", swapChooseWhite);
+document.getElementById("player2ChooseWhite").addEventListener("click", enableBoardInteraction);
+document.getElementById("player2PlaceSwap2").addEventListener("click", swapChoose2);
+document.getElementById("player2PlaceSwap2").addEventListener("click", enableBoardInteraction);
 const gameResult = document.getElementById('gameResult');
-
+const player1Status = document.getElementById('player1Status');
+const player2Status = document.getElementById('player2Status');
+const player1SwapStartMessage = document.getElementById('player1SwapStartMessage');
+const player2SwapStartMessage = document.getElementById('player2SwapStartMessage');
+const player1Swap2Message = document.getElementById('player1Swap2Message');
+const player2Swap2Message = document.getElementById('player2Swap2Message');
+const player1SwapChoose  = document.getElementById('player1SwapChoose');
+const player2SwapChoose = document.getElementById('player2SwapChoose');
+const player1PlaceSwap2 = document.getElementById('player1PlaceSwap2');
+const player2PlaceSwap2 = document.getElementById('player2PlaceSwap2');
 //drawing
 const ctx = boardCanvas.getContext('2d');
 
@@ -20,6 +41,7 @@ const stoneBorderWidth = 1.5;
 const stoneRadius = spacing * 0.45;
 const starPointRadius = 4;
 const coordinatesFontSize = 16;
+
 
 boardCanvas.width = boardSizePx * dpr;
 boardCanvas.height = boardSizePx * dpr;
@@ -110,11 +132,27 @@ let board = [];
 let stone = 1;
 let move = 0;
 let winner = null;
+let turn;
+let swap2 = false;
+let startingPlayer = 1;
 
 function startGame() {
     gameEndModal.close();
     move = 0;
     stone = 1;
+    turn = startingPlayer;
+    swap2 = false;
+    if (turn === 1) {
+        player2Status.classList.remove("highlight");
+        player1Status.classList.add("highlight");
+        player2SwapStartMessage.classList.add("hidden");
+        player1SwapStartMessage.classList.remove("hidden");
+    } else if (turn === 2) {
+        player1Status.classList.remove("highlight");
+        player2Status.classList.add("highlight");
+        player1SwapStartMessage.classList.add("hidden");
+        player2SwapStartMessage.classList.remove("hidden");
+    }
     winner = null;
     board = [];
     for (let row = 0; row < gridSizeLines; row = row + 1) {
@@ -137,21 +175,89 @@ function disableBoardInteraction() {
     boardCanvas.removeEventListener("click", boardClick);
 }
 
+function swapChooseBlack() {
+    player1SwapChoose.classList.add("hidden");
+    player2SwapChoose.classList.add("hidden");
+
+    turn = 3 - turn;
+    if (turn === 1) {
+        player2Status.classList.remove("highlight");
+        player1Status.classList.add("highlight");
+    } else if (turn === 2) {
+        player1Status.classList.remove("highlight");
+        player2Status.classList.add("highlight");
+    }
+}
+
+function swapChooseWhite() {
+    player1SwapChoose.classList.add("hidden");
+    player2SwapChoose.classList.add("hidden");
+}
+
+function swapChoose2() {
+    player1SwapChoose.classList.add("hidden");
+    player2SwapChoose.classList.add("hidden");
+
+    swap2 = true;
+    if (turn === 1) {
+        player1Swap2Message.classList.remove("hidden");
+    } else if (turn === 2) {
+        player2Swap2Message.classList.remove("hidden");
+    }
+}
+
+function updateUI () {
+    if (turn === 1) {
+        player2Status.classList.remove("highlight");
+        player1Status.classList.add("highlight");
+    } else if (turn === 2) {
+        player1Status.classList.remove("highlight");
+        player2Status.classList.add("highlight");
+    }
+
+    if (move === 3 && turn === 1) {
+        player2SwapStartMessage.classList.add("hidden");
+        player1SwapChoose.classList.remove("hidden");
+        player1PlaceSwap2.classList.remove("hidden");
+    } else if (move === 3 && turn === 2) {
+        player1SwapStartMessage.classList.add("hidden");
+        player2SwapChoose.classList.remove("hidden");
+        player2PlaceSwap2.classList.remove("hidden");
+    } else if (move === 5 && swap2 === true && turn === 1) {
+        player1SwapChoose.classList.remove("hidden");
+        player1PlaceSwap2.classList.add("hidden");
+        player2Swap2Message.classList.add("hidden");
+    } else if (move === 5 && swap2 === true && turn === 2) {
+        player2SwapChoose.classList.remove("hidden");
+        player2PlaceSwap2.classList.add("hidden");
+        player1Swap2Message.classList.add("hidden");
+    }
+}
+function updateTurn () {
+    move = move + 1;
+    stone = 3 - stone;
+    if (move >= 3 && swap2 === false || move >= 5) {
+        turn = 3 - turn;
+    }
+}
 function boardClick(click) {
     const rect = boardCanvas.getBoundingClientRect();
     const col = Math.round((click.clientX - rect.left - gridPaddingPx) / spacing);
     const row = Math.round((click.clientY - rect.top - gridPaddingPx) / spacing);
     if (col > -1 && col < gridSizeLines && row > -1 && row < gridSizeLines && board[row][col] === null) {
-        move = move + 1;
         board[row][col] = stone;
         drawStones();
         winDetection(row, col, stone);
-        stone = 3 - stone;
+        updateTurn();
+        updateUI();
+        if (move === 3 || move === 5 && swap2 === true) {
+            disableBoardInteraction();
+        }
     }
     if (move >= gridSizeLines * gridSizeLines) {
         endGame();
     }
-    console.log("move" + move, "stone" + stone);
+    console.log("move" + move, "stone" + stone, "turn" + turn);
     console.table(board);
 }
 
@@ -187,6 +293,7 @@ function winDetection(row, col, stone) {
 
 function endGame() {
     disableBoardInteraction()
+    startingPlayer = 3 - startingPlayer;
     if (winner) {
         console.log("player" + winner + "won");
         gameResult.textContent = "Player " + winner + " Won!";
